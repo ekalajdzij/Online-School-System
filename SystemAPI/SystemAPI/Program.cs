891ed80afb176959efab90a019f98c1823554886
Emir <ekalajdzij1@etf.unsa.ba>
2024-05-23 11:32:40 +0200

    updated program.cs

--- SystemAPI/SystemAPI/Program.cs
+++ SystemAPI/SystemAPI/Program.cs
@@ -1,20 +1,44 @@
 using Microsoft.EntityFrameworkCore;
 using SchoolSystemAPI.Factories;
 using SystemAPI.Data;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Identity.Web;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.OpenApi.Models;
 using Microsoft.Extensions.Azure;
 using SchoolSystemAPI.Services;
+using Microsoft.Identity.Web;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-//Jwt configuration starts here
+// Jwt configuration starts here
 var jwtIssuer = builder.Configuration.GetSection("Jwt:Issuer").Get<string>();
 var jwtKey = builder.Configuration.GetSection("Jwt:Key").Get<string>();
+
+// Add services to the container.
+builder.Services.AddAzureClients(clientBuilder =>
+{
+    clientBuilder.AddBlobServiceClient(builder.Configuration["AzureStorage:blob"]!, preferMsi: true);
+    clientBuilder.AddQueueServiceClient(builder.Configuration["AzureStorage:queue"]!, preferMsi: true);
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddScoped<IAssistantAbstractFactory, AssistantAbstractFactory>();
+builder.Services.AddScoped<IProfessorAbstractFactory, ProfessorAbstractFactory>();
+builder.Services.AddScoped<IStudentAbstractFactory, StudentAbstractFactory>();
+
+// Singleton pattern for BlobStorageService
+builder.Services.AddSingleton<BlobStorageService>(provider =>
+{
+    var configuration = provider.GetRequiredService<IConfiguration>();
+    var logger = provider.GetRequiredService<ILogger<BlobStorageService>>();
+    return BlobStorageService.GetInstance(configuration, logger);
+});
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -38,33 +62,8 @@ builder.Services.AddAuthorization(options =>
     options.AddPolicy("ProfessorOrAssistantOrStudentOrAdmin", policy => policy.RequireRole("Professor", "Assistant", "Student", "Admin"));
     options.AddPolicy("ProfessorOrAssistant", policy => policy.RequireRole("Professor", "Assistant"));
     options.AddPolicy("StudentOrAdmin", policy => policy.RequireRole("Student", "Admin"));
-    options.AddPolicy("AssistantOrAdmin", policy => policy.RequireRole("Asistant", "Admin"));
+    options.AddPolicy("AssistantOrAdmin", policy => policy.RequireRole("Assistant", "Admin"));
     options.AddPolicy("ProfessorOrAdmin", policy => policy.RequireRole("Professor", "Admin"));
-});
-
-builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Add services to the container.
-builder.Services.AddAzureClients(clientBuilder =>
-{
-    clientBuilder.AddBlobServiceClient(builder.Configuration["AzureStorage:blob"]!, preferMsi: true);
-    clientBuilder.AddQueueServiceClient(builder.Configuration["AzureStorage:queue"]!, preferMsi: true);
-});
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
-builder.Services.AddScoped<IAssistantAbstractFactory, AssistantAbstractFactory>();
-builder.Services.AddScoped<IProfessorAbstractFactory, ProfessorAbstractFactory>();
-builder.Services.AddScoped<IStudentAbstractFactory, StudentAbstractFactory>();
-
-//Singleton pattern for BlobStorageService
-builder.Services.AddSingleton<BlobStorageService>(provider =>
-{
-    var configuration = provider.GetRequiredService<IConfiguration>();
-    var logger = provider.GetRequiredService<ILogger<BlobStorageService>>();
-    return BlobStorageService.GetInstance(configuration, logger);
 });
 
 builder.Services.AddCors(options =>
@@ -72,23 +71,18 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAll", buildOptions =>
     {
         buildOptions.AllowAnyHeader()
-               .AllowAnyMethod()
-               .AllowAnyOrigin()
-               .WithExposedHeaders("Authorization");
+                   .AllowAnyMethod()
+                   .AllowAnyOrigin()
+                   .WithExposedHeaders("Authorization");
     });
-});
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "EduSphereAPI", Version = "v1" });
 });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "EduSphereAPI", Version = "v1" });
 
-    // Dodavanje definicije SecurityScheme za JWT tokene
+    // Adding SecurityScheme for JWT tokens
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Description = "JWT Authorization header using the Bearer scheme",
@@ -96,20 +90,20 @@ builder.Services.AddSwaggerGen(c =>
         Scheme = "bearer"
     });
 
-    // Dodavanje globalnih zahteva za sigurnost (Security Requirement) koji koriste JWT tokene
+    // Adding global security requirement using JWT tokens
     c.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
+        {
+            new OpenApiSecurityScheme
             {
-                new OpenApiSecurityScheme
+                Reference = new OpenApiReference
                 {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                },
-                new string[] { }
-            }
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
     });
 });
 
@@ -119,14 +113,6 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseCors("AllowAll");
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
